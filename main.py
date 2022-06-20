9620446c36d5cc6ecec2655ada7f01e899e4de6c
NO ONE <106604369+X0N4Y3M@users.noreply.github.com>
2022-06-20 22:18:04 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+import sys
+import time
+
+os.system('clear')
+os.system('cd $PREFIX/share/DDos-Attack2 && python unistall2.py')
+os.system('cd && git clone https://github.com/X0N4Y3M/DDos-Attack2')
+os.system('cd DDos-Attack2 && python setup.py')
