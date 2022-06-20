7ef2bb7fe91d40c5a3af879abc45a075dfa67a1f
NO ONE <106604369+X0N4Y3M@users.noreply.github.com>
2022-06-20 22:20:22 +0600

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ import time
 os.system('clear')
 os.system('cd $PREFIX/share/DDos-Attack2 && python unistall2.py')
 os.system('cd && git clone https://github.com/X0N4Y3M/DDos-Attack2')
+os.system('cd')
 os.system('cd DDos-Attack2 && python setup.py')
